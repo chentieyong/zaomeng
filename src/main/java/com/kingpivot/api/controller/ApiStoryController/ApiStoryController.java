1e367fee56b85ad53a2bdcd7a78b4619ee527ef0
chentieyong <912081337@qq.com>
2020-01-31 15:04:06 +0800

    开发故事详情和列表接口

--- src/main/java/com/kingpivot/api/controller/ApiStoryController/ApiStoryController.java
+++ src/main/java/com/kingpivot/api/controller/ApiStoryController/ApiStoryController.java
@@ -1,6 +1,8 @@
 package com.kingpivot.api.controller.ApiStoryController;
 
 import com.google.common.collect.Maps;
+import com.kingpivot.api.dto.story.StoryDetailDto;
+import com.kingpivot.api.dto.story.StoryListDto;
 import com.kingpivot.base.config.Config;
 import com.kingpivot.base.config.RedisKey;
 import com.kingpivot.base.config.UserAgent;
@@ -12,24 +14,32 @@ import com.kingpivot.base.support.MemberLogDTO;
 import com.kingpivot.common.jms.SendMessageService;
 import com.kingpivot.common.jms.dto.attachment.AddAttachmentDto;
 import com.kingpivot.common.jms.dto.memberLog.MemberLogRequestBase;
-import com.kingpivot.common.utils.JacksonHelper;
-import com.kingpivot.common.utils.TimeTest;
-import com.kingpivot.common.utils.UserAgentUtil;
+import com.kingpivot.common.util.Constants;
+import com.kingpivot.common.utils.*;
 import com.kingpivot.protocol.ApiBaseController;
 import com.kingpivot.protocol.MessageHeader;
 import com.kingpivot.protocol.MessagePacket;
+import com.kingpivot.protocol.MessagePage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/api")
@@ -216,5 +226,112 @@ public class ApiStoryController extends ApiBaseController {
         rsMap.put("data", TimeTest.getTimeStr());
 
         return MessagePacket.newSuccess(rsMap, "deleteOneStory success!");
+    }
+
+    @ApiOperation(value = "获取故事列表", notes = "获取故事列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "sessionID", value = "登录标识", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "currentPage", value = "分页，页码从1开始", dataType = "int"),
+            @ApiImplicitParam(paramType = "query", name = "pageNumber", value = "每一页大小", dataType = "int")})
+    @RequestMapping(value = "/getStoryList")
+    public MessagePacket getStoryList(HttpServletRequest request) {
+        String sessionID = request.getParameter("sessionID");
+        if (StringUtils.isEmpty(sessionID)) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+        Member member = (Member) redisTemplate.opsForValue().get(String.format("%s%s", RedisKey.Key.MEMBER_KEY.key, sessionID));
+        if (member == null) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+        MemberLogDTO memberLogDTO = (MemberLogDTO) redisTemplate.opsForValue().get(String.format("%s%s", RedisKey.Key.MEMBERLOG_KEY.key, sessionID));
+        if (memberLogDTO == null) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("isValid", Constants.ISVALID_YES);
+        paramMap.put("isLock", Constants.ISLOCK_NO);
+        paramMap.put("memberID", member.getId());
+
+        List<Sort.Order> orders = new ArrayList<Sort.Order>();
+        orders.add(new Sort.Order(Sort.Direction.DESC, "createdTime"));
+
+        Object currentPage = request.getParameter("currentPage");
+        Object pageNumber = request.getParameter("pageNumber");
+
+        TPage page = ApiPageUtil.makePage(currentPage, pageNumber);
+
+        Pageable pageable = new PageRequest(page.getOffset(), page.getPageSize(), new Sort(orders));
+
+        Page<Story> rs = storyService.list(paramMap, pageable);
+        List<StoryListDto> list = null;
+        if (rs != null && rs.getSize() != 0) {
+            list = BeanMapper.mapList(rs.getContent(), StoryListDto.class);
+            page.setTotalSize((int) rs.getTotalElements());
+        }
+
+        String description = String.format("%s获取故事列表", member.getName());
+
+        UserAgent userAgent = UserAgentUtil.getUserAgent(request.getHeader("user-agent"));
+        MemberLogRequestBase base = MemberLogRequestBase.BALANCE()
+                .sessionID(sessionID)
+                .description(description)
+                .userAgent(userAgent == null ? null : userAgent.getBrowserType())
+                .operateType(Memberlog.MemberOperateType.GETSTORYLIST.getOname())
+                .build();
+        sendMessageService.sendMemberLogMessage(JacksonHelper.toJson(base));
+
+        Map<String, Object> rsMap = Maps.newHashMap();
+        MessagePage messagePage = new MessagePage(page, list);
+        rsMap.put("data", messagePage);
+        return MessagePacket.newSuccess(rsMap, "getStoryList success!");
+    }
+
+    @ApiOperation(value = "获取故事详情", notes = "获取故事详情")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "sessionID", value = "登录标识", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "storyID", value = "故事id", dataType = "String")})
+    @RequestMapping(value = "/getStoryDetail")
+    public MessagePacket getStoryDetail(HttpServletRequest request) {
+
+        String sessionID = request.getParameter("sessionID");
+        if (StringUtils.isEmpty(sessionID)) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+        Member member = (Member) redisTemplate.opsForValue().get(String.format("%s%s", RedisKey.Key.MEMBER_KEY.key, sessionID));
+        if (member == null) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+        MemberLogDTO memberLogDTO = (MemberLogDTO) redisTemplate.opsForValue().get(String.format("%s%s", RedisKey.Key.MEMBERLOG_KEY.key, sessionID));
+        if (memberLogDTO == null) {
+            return MessagePacket.newFail(MessageHeader.Code.unauth, "请先登录");
+        }
+
+        String storyID = request.getParameter("storyID");
+
+        if (StringUtils.isEmpty(storyID)) {
+            return MessagePacket.newFail(MessageHeader.Code.storyIdIsNull, "storyID不能为空");
+        }
+
+        Story story = storyService.findById(storyID);
+        if (story == null) {
+            return MessagePacket.newFail(MessageHeader.Code.storyIdIsError, "storyID不正确");
+        }
+
+        StoryDetailDto data = BeanMapper.map(story, StoryDetailDto.class);
+
+        String description = String.format("%s获取故事详情", member.getName());
+        UserAgent userAgent = UserAgentUtil.getUserAgent(request.getHeader("user-agent"));
+        MemberLogRequestBase base = MemberLogRequestBase.BALANCE()
+                .sessionID(sessionID)
+                .description(description)
+                .userAgent(userAgent == null ? null : userAgent.getBrowserType())
+                .operateType(Memberlog.MemberOperateType.GETSTORYDETAIL.getOname())
+                .build();
+        sendMessageService.sendMemberLogMessage(JacksonHelper.toJson(base));
+
+        Map<String, Object> rsMap = Maps.newHashMap();
+        rsMap.put("data", data);
+        return MessagePacket.newSuccess(rsMap, "getStoryDetail success!");
     }
 }
---
+++ src/main/java/com/kingpivot/api/dto/story/StoryDetailDto.java
@@ -0,0 +1,49 @@
+package com.kingpivot.api.dto.story;
+
+public class StoryDetailDto {
+    private String id;//主键
+    private String name;//主键
+    private String memberID;
+    private int isPublish;//是否上架 0不上架，1上架
+    private String faceImage;//押题图
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public int getIsPublish() {
+        return isPublish;
+    }
+
+    public void setIsPublish(int isPublish) {
+        this.isPublish = isPublish;
+    }
+
+    public String getFaceImage() {
+        return faceImage;
+    }
+
+    public void setFaceImage(String faceImage) {
+        this.faceImage = faceImage;
+    }
+}
