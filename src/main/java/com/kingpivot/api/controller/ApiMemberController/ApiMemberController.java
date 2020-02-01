a87e01bbd6b5a9328cd47252e62aada11bc84c12
chentieyong <912081337@qq.com>
2020-02-01 15:37:59 +0800

    开发接口getApplicationMemberList

--- src/main/java/com/kingpivot/api/controller/ApiMemberController/ApiMemberController.java
+++ src/main/java/com/kingpivot/api/controller/ApiMemberController/ApiMemberController.java
@@ -748,7 +748,7 @@ public class ApiMemberController extends ApiBaseController {
         paramMap.put("applicationID", applicationID);
 
         List<Sort.Order> orders = new ArrayList<Sort.Order>();
-        orders.add(new Sort.Order(Sort.Direction.ASC, "orderSeq"));
+        orders.add(new Sort.Order(Sort.Direction.DESC, "createdTime"));
 
         Object currentPage = request.getParameter("currentPage");
         Object pageNumber = request.getParameter("pageNumber");
