54065c5574135c02ed2cff6292ae5f6b6fdfbde6
chentieyong <912081337@qq.com>
2021-10-18 13:58:30 +0800

    会员卡模块接口开发

---
+++ src/main/java/com/kingpivot/api/dto/cardDefine/CardDefineListDto.java
@@ -0,0 +1,94 @@
+package com.kingpivot.api.dto.cardDefine;
+
+public class CardDefineListDto {
+    private String id;//主键
+    private String companyID;//公司ID
+    private String applicationID;
+    private String name;
+    private String shortname;
+    private Integer orderSeq = 1;
+    private String listImage;
+    private String faceImage;
+    private double price = 0d;//申请价格
+    private Integer effectiveDays = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(String companyID) {
+        this.companyID = companyID;
+    }
+
+    public String getApplicationID() {
+        return applicationID;
+    }
+
+    public void setApplicationID(String applicationID) {
+        this.applicationID = applicationID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public void setShortname(String shortname) {
+        this.shortname = shortname;
+    }
+
+    public Integer getOrderSeq() {
+        return orderSeq;
+    }
+
+    public void setOrderSeq(Integer orderSeq) {
+        this.orderSeq = orderSeq;
+    }
+
+    public String getListImage() {
+        return listImage;
+    }
+
+    public void setListImage(String listImage) {
+        this.listImage = listImage;
+    }
+
+    public String getFaceImage() {
+        return faceImage;
+    }
+
+    public void setFaceImage(String faceImage) {
+        this.faceImage = faceImage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getEffectiveDays() {
+        return effectiveDays;
+    }
+
+    public void setEffectiveDays(Integer effectiveDays) {
+        this.effectiveDays = effectiveDays;
+    }
+}
---
+++ src/main/java/com/kingpivot/api/dto/memberCard/MemberCardListDto.java
@@ -0,0 +1,91 @@
+package com.kingpivot.api.dto.memberCard;
+
+import com.kingpivot.common.utils.TimeTest;
+
+import java.sql.Timestamp;
+
+public class MemberCardListDto {
+    private String id;//主键
+    private String applicationID;
+    private String name;
+    private String memberID;
+    private String cardDefineID;
+    private Timestamp beginTime;
+    private String beginTimeStr;
+    private Timestamp endTime;
+    private String endTimeStr;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApplicationID() {
+        return applicationID;
+    }
+
+    public void setApplicationID(String applicationID) {
+        this.applicationID = applicationID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getCardDefineID() {
+        return cardDefineID;
+    }
+
+    public void setCardDefineID(String cardDefineID) {
+        this.cardDefineID = cardDefineID;
+    }
+
+    public Timestamp getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Timestamp beginTime) {
+        this.beginTime = beginTime;
+        this.beginTimeStr = TimeTest.toDateTimeFormat(beginTime);
+    }
+
+    public String getBeginTimeStr() {
+        return beginTimeStr;
+    }
+
+    public void setBeginTimeStr(String beginTimeStr) {
+        this.beginTimeStr = beginTimeStr;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+        this.endTimeStr = TimeTest.toDateTimeFormat(endTime);
+    }
+
+    public String getEndTimeStr() {
+        return endTimeStr;
+    }
+
+    public void setEndTimeStr(String endTimeStr) {
+        this.endTimeStr = endTimeStr;
+    }
+}
