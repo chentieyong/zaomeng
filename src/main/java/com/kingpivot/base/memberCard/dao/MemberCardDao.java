66283dab63c780ef2a3fbc8a488da9c1b7a48e1c
chentieyong <912081337@qq.com>
2021-10-20 16:13:42 +0800

    会员价报错修改

--- src/main/java/com/kingpivot/base/memberCard/dao/MemberCardDao.java
+++ src/main/java/com/kingpivot/base/memberCard/dao/MemberCardDao.java
@@ -13,6 +13,6 @@ import javax.persistence.Table;
 @Qualifier("memberCardDao")
 public interface MemberCardDao extends BaseDao<MemberCard, String> {
 
-    @Query(value = "SELECT * FROM memberCard WHERE isValid=1 AND isLock=0 AND memberID=?1 NOW() BETWEEN beginTime AND endTime ORDER BY createdTime DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM memberCard WHERE isValid=1 AND isLock=0 AND memberID=?1 AND NOW() BETWEEN beginTime AND endTime ORDER BY createdTime DESC", nativeQuery = true)
     MemberCard getEffectiveMemberCard(String memberID);
 }
